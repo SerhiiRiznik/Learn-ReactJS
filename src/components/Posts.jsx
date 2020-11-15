2652d9c21cf4ec582b4512e3aff986bab56e8a96
SerhiiRiznik <69801777+SerhiiRiznik@users.noreply.github.com>
2020-11-15 17:22:28 +0100

    V1 (add module.css)

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,19 +1,22 @@
+// import './Posts.modul.css';
+import posts from './Posts.module.css';
+
 function Posts() {
    return (
-      <div className='user__posts'>
-         <div className='user__new-post'>
+      <div className={posts.user__posts}>
+         <div className={posts.user__new_post}>
             <h2>New post</h2>
             <textarea></textarea>
             <button type="submit" value="Sent">Submit</button>
          </div>
-         <div className='user__posts'>
+         <div className={posts.user__posts}>
             <h2>All Posts</h2>
-            <div className='post__item'>
+            <div className={posts.item}>
                <p>
                   here new post 1
             </p>
             </div>
-            <div className='post__item'>
+            <div className={posts.item}>
                <p>
                   here new post 2
             </p>
